072ff7c62b73e44296817276793276d52a91176f
Edmond Low <edmondlowjy@gmail.com>
2018-06-28 13:31:55 +0800

    test commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## In this assignment, I realized that the nested function 'set' is not invoked and hence
+# does not serve any purpose in the code. Thus, I have removed it entirely.
 
-## Write a short comment describing this function
+
+
+## makeCacheMatrix creates a list that stores the input matrix and corresponding inverse
+# if the inverse has been solved once before
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m.inv <- NULL
+  get <- function() x
+  setinv <- function(inv) m.inv <<- inv
+  getinv <- function() m.inv
+  list(get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve modifies the list created by makeCacheMatrix by calculating the inverse of the
+# matrix contained within the list and storing the inverse within this list
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m.inv <- x$getinv()
+  if(!is.null(m.inv)){
+    message("getting cached data")
+    return(m.inv)
+  }
+  mat.data <- x$get()
+  m.inv <- solve(mat.data)
+  x$setinv(m.inv)
+  m.inv
 }
+
+## Test below
+# First run of cacheSolve(test) should store inverse. Second run should display cache message
+
+mat.test=matrix(c(-1,1,1.5,-1),2,2)
+test=makeCacheMatrix(mat.test)
+cacheSolve(test)
+
